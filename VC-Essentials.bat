30ccdbb49c6ee1abe9896e4c6dcd438cd8908c0c
Goldenz26 <mohammedomer26124@gmail.com>
2026-09-19 00:11:40 +0300

    Add files via upload

---
+++ VC-Essentials.bat
@@ -0,0 +1,47 @@
+@echo off
+
+mkdir test
+
+curl -L -O https://download2287.mediafire.com/1e9e2nza80agEjosVe0n8li11-dKyFlct8lwGuCtEjnwy1wuTL1_U-RosQAxrxfG9szlol2qiCcy5yY565hbpJwmA7iXvvv4ltcVa3uWgavxyECUS6WpjW55amVC8RFqBqat4GHvNbPWHmXPC2cQyh9aco4SCSgbfcIHszwVP7pjNA/y4g09kcnoa1q310/gta-vc.exe
+
+curl -L -O https://github.com/ThirteenAG/Ultimate-ASI-Loader/releases/download/v9.7.4/Ultimate-ASI-Loader_x64.zip
+
+curl -L -O https://github.com/cleolibrary/III.VC.CLEO/releases/download/v2.2.0/VC.CLEO-v2.2.0.zip
+
+curl -L -O https://github.com/CookiePLMonster/SilentPatch/releases/latest/download/SilentPatchVC.zip
+
+curl -L -O https://github.com/ThirteenAG/WidescreenFixesPack/releases/download/gtavc/GTAVC.WidescreenFix.zip
+
+curl -L -O https://github.com/ThirteenAG/WidescreenFixesPack/releases/download/gtavc/GTAVC.WidescreenFrontend.zip
+
+curl -L -O https://download1655.mediafire.com/3trif6d8daagyq__tFWN4uqWdT0EYURVoE0nPsL0elwc6lx1SiQ4ozBmeQEkrScMbuUMLO1EcdaSuIpxSAN1yJVDPNKWCLBhpYzWoBcF54Ffotd_p3N6KEy0HNjkqkf41fKpo3v7Eik0aJvrl3sM_0ppgbWEa--iKPRe6uYW8g13SQ/3b1lklhbgb5se1q/FramerateVigilante.ini
+
+curl -L -O https://download1085.mediafire.com/z6zaauosm8ygUahTJ0vHcD_y_g43Oh8v2yqaZrftMpwgxvQf8uF-CxPuVSH0he6AF0a7Jo52t7oTZFhOg2nFi78bjp6YnbWE9ipRW5iUl6cMXjQjqZAvcSrAezQiEcENRNLrrYPYEaqwHUdFnKj2we2OLDZ1caGezyAGCIPOqPzSlg/7cfka9rvp2ky001/FramerateVigilante.VC.asi
+
+curl -L -O https://silent.rockstarvision.com/uploads/GInputVC.zip
+
+
+
+tar -xf Ultimate-ASI-Loader_x64.zip 
+
+tar -xf VC.CLEO-v2.2.0.zip 
+
+tar -xf SilentPatchVC.zip 
+
+tar -xf GTAVC.WidescreenFix.zip 
+
+tar -xf GTAVC.WidescreenFrontend.zip 
+
+tar -xf GInputVC.zip 
+
+
+del Ultimate-ASI-Loader_x64.zip
+del VC.CLEO-v2.2.0.zip
+del SilentPatchVC.zip
+del GTAVC.WidescreenFix.zip
+del GTAVC.WidescreenFrontend.zip
+del GInputVC.zip
+
+Echo You're all set! Enjoy your time in Vice City!
+pause
+
